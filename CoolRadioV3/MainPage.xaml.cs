18ad9a063cf471d1b754dbea1334c6f83ba4b66e
gxsham <gxsham@gmail.com>
2016-08-27 12:12:41 +0300

    initial

---
+++ CoolRadioV3/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CoolRadioV3
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public SongInfo songInfo;
+		public MainPage()
+		{
+			this.InitializeComponent();
+			CheckTrack();
+		}
+
+		private void Play_Click(object sender, RoutedEventArgs e)
+		{
+			mediaElement.Volume = 1;
+			Play.Visibility = Visibility.Collapsed;
+			Stop.Visibility = Visibility.Visible;
+		}
+
+		private void Stop_Click(object sender, RoutedEventArgs e)
+		{
+			mediaElement.Volume = 0;
+			Stop.Visibility = Visibility.Collapsed;
+			Play.Visibility = Visibility.Visible;
+		}
+
+		public async void CheckTrack()
+		{
+			var client = new HttpClient();
+			var link = "http://coolradio.md/checkTrack";
+			string lastSong = "";
+			string checkTrack;
+			while (true)
+			{
+
+				checkTrack = client.GetStringAsync(link).Result;
+				if (lastSong != checkTrack)
+				{
+					//change song name on screen
+					songInfo = JsonConvert.DeserializeObject<SongInfo>(checkTrack);
+					if (!string.IsNullOrEmpty(songInfo.Cover))
+					{
+						CurrentSong.Text = $"{songInfo.Track} - {songInfo.Artist}";
+						var coverLink = songInfo.Cover;
+						Cover.Source = new BitmapImage(new Uri(coverLink, UriKind.RelativeOrAbsolute));
+					}
+				}
+				await Task.Delay(5000);
+				lastSong = checkTrack;
+			}
+		}
+	}
+}
---
+++ CoolRadioV3/SongInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolRadioV3
+{
+	public class SongInfo
+	{
+		public string Track { get; set; }
+		public string Artist { get; set; }
+		public string Cover { get; set; }
+	}
+}
